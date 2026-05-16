7724dd97a4d9e0f1959ffb7dd43ee4353bdcb824
guisalmeida <guisalmeida.dev@gmail.com>
2026-05-16 01:54:51 +0100

    refactor: use boardcontroller on view layer

--- src/main/java/com/guisalmeida/eightpuzzle/view/swing/button/MoveUpButton.java
+++ src/main/java/com/guisalmeida/eightpuzzle/view/swing/button/MoveUpButton.java
@@ -2,16 +2,16 @@ package com.guisalmeida.eightpuzzle.view.swing.button;
 
 import java.awt.event.ActionEvent;
 
+import com.guisalmeida.eightpuzzle.control.BoardController;
 import com.guisalmeida.eightpuzzle.model.Player;
-import com.guisalmeida.eightpuzzle.model.Board;
 import com.guisalmeida.eightpuzzle.view.swing.view.ControlView;
 import com.guisalmeida.eightpuzzle.view.swing.view.BoardView;
 
 public class MoveUpButton extends AbstractMoveButton {
 
-	public MoveUpButton(String direction, Board board,
+	public MoveUpButton(String direction, BoardController boardController,
 		BoardView boardView, ControlView controlView, Player player) {
-		super(direction, board, boardView, controlView, player);
+		super(direction, boardController, boardView, controlView, player);
 	}
 
 	@Override
@@ -21,9 +21,8 @@ public class MoveUpButton extends AbstractMoveButton {
 
 	@Override
 	public void updateBoardState() {
-		this.getBoardController().moveUp();
-		this.getBoardView().updateBoardView(this.getBoard());
-		Integer moves = this.getPlayer().getMoves();
-		this.getPlayer().setMoves(moves + 1);
-	};
+		getBoardController().moveUp();
+		getBoardView().updateBoardView(getBoardController().getBoard());
+		getPlayer().setMoves(getPlayer().getMoves() + 1);
+	}
 }
--- src/main/java/com/guisalmeida/eightpuzzle/view/swing/view/ControlView.java
+++ src/main/java/com/guisalmeida/eightpuzzle/view/swing/view/ControlView.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.guisalmeida.eightpuzzle.control.BoardController;
 import com.guisalmeida.eightpuzzle.model.Board;
 import com.guisalmeida.eightpuzzle.model.Player;
 import com.guisalmeida.eightpuzzle.model.BoardObserver;
@@ -20,7 +21,7 @@ import com.guisalmeida.eightpuzzle.view.swing.button.MoveRightButton;
 import com.guisalmeida.eightpuzzle.view.swing.button.MoveLeftButton;
 
 public class ControlView extends JPanel implements KeyListener, BoardObserver {
-	private final Board board;
+	private final BoardController boardController;
 	private final Player player;
 	private final BoardView boardView;
 	private JLabel movesLabel;
@@ -29,13 +30,13 @@ public class ControlView extends JPanel implements KeyListener, BoardObserver {
 	private MoveRightButton buttonRight;
 	private MoveLeftButton buttonLeft;
 
-	public ControlView(Board board, BoardView boardView, Player player) {
-		this.board = board;
+	public ControlView(BoardController boardController, BoardView boardView, Player player) {
+		this.boardController = boardController;
 		this.boardView = boardView;
 		this.player = player;
 
 		generateControlLayout();
-		this.board.registerObserver(this);
+		this.boardController.getBoard().registerObserver(this);
 	}
 
 	private void generateControlLayout() {
@@ -59,22 +60,22 @@ public class ControlView extends JPanel implements KeyListener, BoardObserver {
 		position.gridwidth = 1;
 		position.gridy = 0;
 		position.gridx = 5;
-		buttonUp = new MoveUpButton("↑", board, boardView, this, player);
+		buttonUp = new MoveUpButton("↑", boardController, boardView, this, player);
 		add(buttonUp, position);
 
 		position.gridy = 2;
 		position.gridx = 5;
-		buttonDown = new MoveDownButton("↓", board, boardView, this, player);
+		buttonDown = new MoveDownButton("↓", boardController, boardView, this, player);
 		add(buttonDown, position);
 
 		position.gridy = 1;
 		position.gridx = 6;
-		buttonRight = new MoveRightButton("→", board, boardView, this, player);
+		buttonRight = new MoveRightButton("→", boardController, boardView, this, player);
 		add(buttonRight, position);
 
 		position.gridy = 1;
 		position.gridx = 4;
-		buttonLeft = new MoveLeftButton("←", board, boardView, this, player);
+		buttonLeft = new MoveLeftButton("←", boardController, boardView, this, player);
 		add(buttonLeft, position);
 	}
 
@@ -101,36 +102,16 @@ public class ControlView extends JPanel implements KeyListener, BoardObserver {
 	@Override
 	public void onBoardStateChanged(Board board) {
 		checkGameOver(board);
-	};
+	}
 
 	@Override
 	public void keyPressed(KeyEvent event) {
-		Map<Integer, Runnable> keyMap = new HashMap<Integer, Runnable>();
+		Map<Integer, Runnable> keyMap = new HashMap<>();
 
-		keyMap.put(KeyEvent.VK_DOWN, new Runnable() {
-			@Override
-			public void run() {
-				buttonDown.updateBoardState();
-			}
-		});
-		keyMap.put(KeyEvent.VK_UP, new Runnable() {
-			@Override
-			public void run() {
-				buttonUp.updateBoardState();
-			}
-		});
-		keyMap.put(KeyEvent.VK_RIGHT, new Runnable() {
-			@Override
-			public void run() {
-				buttonRight.updateBoardState();
-			}
-		});
-		keyMap.put(KeyEvent.VK_LEFT, new Runnable() {
-			@Override
-			public void run() {
-				buttonLeft.updateBoardState();
-			}
-		});
+		keyMap.put(KeyEvent.VK_UP, () -> buttonUp.updateBoardState());
+		keyMap.put(KeyEvent.VK_DOWN, () -> buttonDown.updateBoardState());
+		keyMap.put(KeyEvent.VK_RIGHT, () -> buttonRight.updateBoardState());
+		keyMap.put(KeyEvent.VK_LEFT, () -> buttonLeft.updateBoardState());
 
 		Runnable action = keyMap.get(event.getKeyCode());
 		if (action != null) {
@@ -140,7 +121,7 @@ public class ControlView extends JPanel implements KeyListener, BoardObserver {
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-	};
+	}
 
 	@Override
 	public void keyTyped(KeyEvent event) {
